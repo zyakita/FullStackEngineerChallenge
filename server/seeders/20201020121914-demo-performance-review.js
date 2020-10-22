5d6b2b72ba736d3af5bb137d640f1137c42c0a59
Phong Tran <phong.bongmay@gmail.com>
2020-10-22 14:29:39 +0900

    change seeders: userId from 45~50 for Performance Reviews

--- server/seeders/20201020121914-demo-performance-review.js
+++ server/seeders/20201020121914-demo-performance-review.js
@@ -5,7 +5,7 @@ let seedPRs = [];
 for (let i = 1; i <= 20; i++) {
   seedPRs.push({
     id: i,
-    userId: Math.floor(Math.random() * (10 - 2 + 1) + 2), // random between 2~10
+    userId: Math.floor(Math.random() * (50 - 40 + 1) + 40), // random between 40~50
     title: `Performance review example ${i}`,
     description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dignissim nisi non nunc ultrices tristique. Integer non ex turpis. Aliquam aliquet lacus ut leo lacinia cursus. Sed dapibus, lectus quis tempus condimentum, sapien lectus iaculis magna, non sollicitudin risus turpis a tellus. In hac habitasse platea dictumst. Praesent neque diam, cursus vitae pharetra at, ultricies non erat. Fusce rutrum, arcu et sodales convallis, velit magna sollicitudin magna, ac facilisis nibh erat vitae lacus. Mauris sed sapien nibh. Vivamus at ultricies est.`,
     createdAt: new Date(),
